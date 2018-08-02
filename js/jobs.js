5641e8615c51bccc73b7c5a304091787d7c127b8
Austin Pierson <apierson.office@gmail.com>
2018-08-01 18:10:25 -0600

    Job Added

--- js/jobs.js
+++ js/jobs.js
@@ -2,8 +2,7 @@ new Vue({
 	el: '#app',
 	data: {
 		jobs: [
-			//ADDJOB// 
-			{ href: 'https://remoteok.io/remote-jobs/68319-remote-senior-backend-engineer-hooked', title: 'Senior Backend Engineer', company: 'Hooked', date: 'August 01, 2018', }, 
+			//ADDJOB//  
 
 		],
 	},
