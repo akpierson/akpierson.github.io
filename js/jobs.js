9fcb9163a84f089c526e44e5e9b694c2a38ef298
Austin Pierson <apierson.office@gmail.com>
2018-08-01 18:17:03 -0600

    Job Added

--- js/jobs.js
+++ js/jobs.js
@@ -3,6 +3,7 @@ new Vue({
 	data: {
 		jobs: [
 			//ADDJOB// 
+			{ href: 'https://weworkremotely.com/remote-jobs/ergeon-inc-sales-manager', title: 'Sales Manager', company: 'Ergeon Inc.', date: 'August 01, 2018', }, 
 			{ href: 'https://remoteok.io/remote-jobs/68319-remote-senior-backend-engineer-hooked', title: 'Senior Backend Engineer', company: 'Hooked', date: 'August 01, 2018', },  
 
 		],
